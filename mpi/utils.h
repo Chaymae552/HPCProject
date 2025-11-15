2bdcd9146f43780df0bb44a3d8e18ee592699ebc
chaymae Ed-dyb <chaymaeed571@gmail.com>
2025-11-15 18:17:32 +0000

    utils.h

--- mpi/utils.h
+++ mpi/utils.h
@@ -4,7 +4,7 @@
 #include <stddef.h>
 #include <stdint.h>
 
-/* Learning-rate schedule enum (for possible extensions) */
+/* Learning-rate schedule enum */
 typedef enum {
     LR_CONST = 0,
     LR_TIME  = 1,
@@ -12,7 +12,7 @@ typedef enum {
     LR_STEP  = 3
 } lr_schedule_t;
 
-/* Activation enum shared by everything (MLP + helpers) */
+/* Activation enum shared by everything */
 typedef enum {
     ACT_TANH  = 0,
     ACT_RELU  = 1,
@@ -27,28 +27,22 @@ void *xcalloc(size_t n, size_t sz);
 /* ----- timing ----- */
 uint64_t now_millis(void);
 
-/* ----- shuffling ----- */
 void make_permutation(int *perm, int n, unsigned seed);
 
-/* ----- dataset loader -----
-   dir must contain: data_X.txt, data_y.txt
-*/
 int load_dataset(const char *dir,
                  double **X_out, int **y_out,
                  int *N_out, int *D_out, int *C_out);
 
-/* ----- softmax (1 vector) ----- */
 void softmax(const double *logits, int K, double *probs);
 
-/* ----- negative log likelihood from probs ----- */
 double nll_from_probs(const double *p, int y);
 
 /* ----- activations ----- */
 double act_forward(double z, act_t a);
 double act_backward(double z, act_t a);
 
-/* ----- generic LR schedule (unused in current MPI main but available) ----- */
+/* ----- LR schedule ----- */
 double lr_value(lr_schedule_t sched, double lr0, double decay,
                 int step_every, int t, double gamma);
 
-#endif /* UTILS_H */
+#endif
